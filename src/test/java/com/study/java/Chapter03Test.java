fb391d95eb1da7a40acdc23c993acbcce8b8ab86
khghouse <khghouse@naver.com>
2022-08-03 23:55:28 +0900

    Predicate

--- src/test/java/com/study/java/Chapter03Test.java
+++ src/test/java/com/study/java/Chapter03Test.java
@@ -5,12 +5,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.study.java.interfaces.BufferedReaderProcessor;
+import com.study.java.interfaces.Predicate;
+import com.study.java.model.Apple;
 
 @ExtendWith(MockitoExtension.class)
 public class Chapter03Test {
@@ -31,6 +35,15 @@ public class Chapter03Test {
 	void processFileTwoLines() throws Exception {
 		String result = this.processFile((BufferedReader br) -> br.readLine() + br.readLine());
 		assertThat(result).isEqualTo("안녕하세요");
+	}
+
+	@Test
+	void predicate() {
+		List<String> list = Arrays.asList("", "", "안녕", "", "", "자바8");
+
+		Predicate<String> nonEmptyStringPredicate = (String s) -> !s.isEmpty();
+		List<String> nonEmpty = Apple.filter(list, nonEmptyStringPredicate);
+		System.out.println(nonEmpty);
 	}
 
 	public String processFile() throws IOException {
